c43a4d06c2ee154a53301ff491782ce3065fe234
ChetMokh <aaria@gmail.com>
2015-11-20 19:44:28 -0800

    Starting Project Controller

---
+++ GameController 2 2/app/src/main/java/edu/csumb/gamecontroller/MainActivity.java
@@ -0,0 +1,194 @@
+package edu.csumb.gamecontroller;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import org.json.JSONObject;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private IOSocket socket;
+    private SensorManager mSensorManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        connect();
+
+        setContentView(R.layout.activity_main);
+
+        final Button firebutton = (Button) findViewById(R.id.fireButton);
+
+        firebutton.setOnTouchListener(
+                new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+
+                        sendFire();
+                        Log.i("AIPSERVER", "Message sent to server: fire!");
+
+
+                        return true;
+
+                    }
+                }
+
+        );
+
+        final SeekBar speedSlider = (SeekBar) findViewById(R.id.speed);
+
+        speedSlider.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        sendSpeed(progress);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mSensorManager.registerListener(this,
+                mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+        // for the system's orientation sensor registered listeners
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // to stop the listener and save battery
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        sendOrientation(event.values[0],event.values[1], event.values[2]);
+        // get the angle around the z-axis rotated
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // not in use
+    }
+
+    void sendFire() {
+        // get the angle around the z-axis rotated
+        try {
+
+            JSONObject message = new JSONObject(new String("{}"));
+
+            if (socket.isConnected()) {
+                socket.emit("fire", message);
+                Log.i("AIPSERVER", "Message sent to server: fire!");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    void sendSpeed(int speed) {
+        try {
+
+            JSONObject message = new JSONObject(new String("{speed:"+ speed/10f+"}"));
+
+            if (socket.isConnected()) {
+                socket.emit("speed", message);
+                Log.i("AIPSERVER", "Message sent to server: speed");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    void sendOrientation(float deltaAlpha, float deltaBeta, float deltaGamma) {
+        try {
+
+
+            String messageContent = new String("{alpha: "+deltaAlpha+ ",beta: " + deltaBeta +", gamma:" + deltaGamma+ "}");
+
+            JSONObject message = new JSONObject(messageContent);
+
+            if (socket.isConnected()) {
+                socket.emit("orientation", message);
+                Log.i("AIPSERVER", "Message sent to server: " + message.getString("alpha"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    void connect() {
+
+        socket = new IOSocket("http://aipservers.com:3000", new MessageCallback() {
+
+            @Override
+            public void onMessage(String message) {
+                // Handle simple messages
+            }
+
+            @Override
+            public void onConnect() {
+                // Socket connection opened
+            }
+
+            @Override
+            public void onDisconnect() {
+                // Socket connection closed
+            }
+
+            @Override
+            public void on(String event, JSONObject... data) {
+
+            }
+
+            @Override
+            public void onMessage(JSONObject json) {
+
+            }
+
+            @Override
+            public void onConnectFailure() {
+
+            }
+        });
+
+        socket.connect();
+    }
+}
